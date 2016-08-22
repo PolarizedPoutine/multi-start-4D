4c97b55a30c9ef20560a02864799e235d5ec2117
PolarizedPoutine <ali.ramadhan@uwaterloo.ca>
2016-08-22 13:01:53 -0400

    Does MATLAB see this now?

--- HamiltonsEquations4Ion.m
+++ HamiltonsEquations4Ion.m
@@ -1,4 +1,4 @@
-function out = hamiltonsEquations4Ion(time, p)
+function out = hamiltonsEquations4Ion(time, p) # Git doesn't see this???
 % Inputs:
 % * time: a 1x2 row vector containing [initialTime, finalTime].
 % * par : a 1x32 row vector containing position, momemtum, mass, and charge
